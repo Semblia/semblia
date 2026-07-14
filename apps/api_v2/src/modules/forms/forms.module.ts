8d7ba8973006cd25b2343978b7f1c47205b1c2a7
Anubhab Debnath <anubhabdebnath98@gmail.com>
2026-07-15 01:48:24 +0530

    fix(forms): scope runtime snapshots to signed hosts

--- apps/api_v2/src/modules/forms/forms.module.ts
+++ apps/api_v2/src/modules/forms/forms.module.ts
@@ -1,20 +1,14 @@
 import { Module } from "@nestjs/common";
 import { AuthzModule } from "../../common/authz/authz.module.js";
 import { BillingModule } from "../billing/billing.module.js";
+import { PublicSurfacesModule } from "../public-surfaces/public-surfaces.module.js";
 import { FormsController } from "./forms.controller.js";
-import {
-  RuntimeFormsController,
-  RuntimeSnapshotsController,
-} from "./runtime-forms.controller.js";
+import { RuntimeFormsController } from "./runtime-forms.controller.js";
 import { FormsService } from "./forms.service.js";
 
 @Module({
-  imports: [AuthzModule, BillingModule],
-  controllers: [
-    FormsController,
-    RuntimeFormsController,
-    RuntimeSnapshotsController,
-  ],
+  imports: [AuthzModule, BillingModule, PublicSurfacesModule],
+  controllers: [FormsController, RuntimeFormsController],
   providers: [FormsService],
 })
 export class FormsModule {}
